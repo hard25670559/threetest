ea60bfccbfa1a83327ce69fb52c1638c84453905
hardanonymous <hard25670559@gmail.com>
2020-04-28 15:25:23 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import * as Three from 'three'
+
+let camera, scene, renderer;
+let geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+    camera = new Three.PerspectiveCamera(
+        70, 
+        window.innerWidth / window.innerHeight, 
+        0.01, 
+        10
+    );
+    camera.position.z = 1;
+
+    scene = new Three.Scene();
+
+    geometry = new Three.BoxGeometry(0.2, 0.2, 0.2, 0.2);
+    material = new Three.MeshNormalMaterial();
+
+    mesh = new Three.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new Three.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.append(renderer.domElement);
+}
+
+function animate() {
+
+}
